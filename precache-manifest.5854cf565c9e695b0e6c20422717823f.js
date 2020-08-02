b19d979c1a90801d7378857cfff38891c60cd742
Nina Loeterman <nina.loeterman@gmail.com>
2020-08-02 14:14:36 +0300

    Updates

---
+++ precache-manifest.5854cf565c9e695b0e6c20422717823f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4aa197cf1e528492a01ee991ef25133",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f80ee49ca72cf8059ae9",
+    "url": "/react-todo-app/static/css/main.820e9df6.chunk.css"
+  },
+  {
+    "revision": "593add3a186325e5b2f2",
+    "url": "/react-todo-app/static/js/2.77c35ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.77c35ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80ee49ca72cf8059ae9",
+    "url": "/react-todo-app/static/js/main.250190e4.chunk.js"
+  },
+  {
+    "revision": "3d1e41507955c947834a",
+    "url": "/react-todo-app/static/js/runtime-main.b9410e1b.js"
+  }
+]);
