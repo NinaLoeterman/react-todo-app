3c41994e350c98bdaed9e89e28d98f545965f0ea
Nina Loeterman <nina.loeterman@gmail.com>
2020-08-02 13:03:48 +0300

    file organization

---
+++ build/precache-manifest.13d7fc0579a61f86a5313771109462ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c89a51aadee3450d4a2e3ae72e2027a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "390d1fc96b58a0abf733",
+    "url": "/static/css/main.820e9df6.chunk.css"
+  },
+  {
+    "revision": "593add3a186325e5b2f2",
+    "url": "/static/js/2.77c35ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77c35ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390d1fc96b58a0abf733",
+    "url": "/static/js/main.d763d7e2.chunk.js"
+  },
+  {
+    "revision": "e90a02fb6b6a3d49eb98",
+    "url": "/static/js/runtime-main.70aaa4c8.js"
+  }
+]);
